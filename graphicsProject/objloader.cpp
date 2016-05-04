b4f50941998b8dc5a5fbb62313771ac05cbf099f
Adham Zahran <adham.zahran@hotmail.com>
2016-05-04 15:14:59 +0200

    removed .obj from the .gitignore

--- graphicsProject/objloader.cpp
+++ graphicsProject/objloader.cpp
@@ -34,7 +34,7 @@ bool loadOBJ(
 	FILE * file = fopen(path, "r");
 	if( file == NULL ){
 		printf("Impossible to open the file ! Are you in the right path ? See Tutorial 1 for details\n");
-		getchar();
+        //getchar();
 		return false;
 	}
 
